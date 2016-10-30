472fe6f7ea53e11166a6a234dc17a0880a5e59a1
Andrew Chen <andrew.chuanye.chen@gmail.com>
2016-10-30 16:14:38 +1300

    Add fn lock led

--- keyboard/gh60chn/led.c
+++ keyboard/gh60chn/led.c
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void led_set(uint8_t usb_led)
 {
+/*
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
         // output low
         DDRB |= (1<<2);
@@ -31,4 +32,5 @@ void led_set(uint8_t usb_led)
         DDRB &= ~(1<<2);
         PORTB &= ~(1<<2);
     }
+*/
 }
