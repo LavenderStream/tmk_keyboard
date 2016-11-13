1eb99627e24705670745867eca13ed1bbb1d97ee
Andrew Chen <andrew.chuanye.chen@gmail.com>
2016-11-13 18:39:32 +1300

    Revert "Vim style arrow keys"
    This reverts commit 72d079d2b12b1375ccb5c9b5f7d2d0fb7efcab61.

--- keyboard/gh60chn/keymap.c
+++ keyboard/gh60chn/keymap.c
@@ -5,9 +5,15 @@
 #define SHIFT_LAYER 1
 #define FN_LAYER 2
 
+enum function_id {
+    FN_KEY_DEFAULT_LAYER,
+    FN_KEY_FN_LAYER,
+};
+
 const action_t PROGMEM fn_actions[32] = {
-    [0] = ACTION_LAYER_MOMENTARY(FN_LAYER),
-    [1] = ACTION_LAYER_MODS(SHIFT_LAYER, MOD_LSFT),
+    [0] = ACTION_FUNCTION(FN_KEY_DEFAULT_LAYER),
+    [1] = ACTION_FUNCTION(FN_KEY_FN_LAYER),
+    [2] = ACTION_LAYER_MODS(SHIFT_LAYER, MOD_LSFT),
     [5] = ACTION_BACKLIGHT_TOGGLE(),
     [6] = ACTION_BACKLIGHT_DECREASE(),
     [7] = ACTION_BACKLIGHT_INCREASE(),
@@ -19,8 +25,8 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
         FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
-        FN1, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     LEFT,DOWN,UP,  RGHT),
+        FN2, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          UP,   \
+        LCTL,LGUI,LALT,          SPC,                     FN0, LEFT,DOWN,RGHT),
     /* 1: Shift layer: Shift + Esc -> Tilde */
     [SHIFT_LAYER] = KEYMAP_ANSI(
         GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
@@ -32,10 +38,58 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FN_LAYER] = KEYMAP_ANSI(
         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,  \
         NO,  NO,  UP,  NO,  NO,  NO,  NO,  NO,  INS, NO,  PSCR,SLCK,PAUS,NO,   \
-        TRNS,LEFT,DOWN,RGHT,NO,  NO,  LEFT,DOWN,UP,  RGHT,HOME,PGUP,     TRNS, \
-        TRNS,NO,  NO,  FN6, FN5, FN7, NO,  NO,  NO,  END, PGDN,          TRNS, \
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+        FN1, LEFT,DOWN,RGHT,NO,  NO,  LEFT,DOWN,UP  ,RGHT,HOME,PGUP,     TRNS, \
+        TRNS,NO,  NO,  FN6, FN5, FN7, NO,  NO,  NO,  END, PGDN,          RSFT, \
+        TRNS,TRNS,TRNS,          TRNS,                    FN1, RALT,RGUI,RCTL),
 };
+
+inline static void layer_set(uint8_t layer, bool state) {
+    if (state) layer_on(layer);
+    else layer_off(layer);
+}
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    static bool fn_locked = false;
+    bool pressed = record->event.pressed;
+
+    switch (id) {
+    case FN_KEY_DEFAULT_LAYER:
+        if (!fn_locked) {
+            layer_set(FN_LAYER, pressed);
+        } else {
+            if (pressed) {
+                fn_locked = false;
+                layer_off(FN_LAYER);
+                // LED off
+                DDRB &= ~(1<<2);
+                PORTB &= ~(1<<2);
+            }
+        }
+        break;
+    case FN_KEY_FN_LAYER:
+        if (!fn_locked) {
+            if (pressed) {
+                fn_locked = true;
+                layer_on(FN_LAYER);
+                // LED on
+                DDRB |= (1<<2);
+                PORTB &= ~(1<<2);
+            }
+        } else {
+            layer_set(FN_LAYER, !pressed);
+        }
+        break;
+    }
+}
+
+/*
+void hook_late_init(void) {
+    xprintf("hook_late_init: %d\n", layer_state);
+}
+void hook_layer_change(uint32_t layer_state) {
+    xprintf("hook_layer_change: %d\n", layer_state);
+}
+*/
 
 /*
     KEYMAP_ANSI(
